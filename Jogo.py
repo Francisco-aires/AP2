d051765ba02bf0146fc0b6d6f191a7ed0f35c96d
Matheus Mizrahi <matheusm3@al.insper.edu.br>
2024-04-29 10:43:30 -0300

    Escolha do país de guerra

--- Jogo.py
+++ Jogo.py
@@ -77,5 +77,21 @@ print("O computador já está alocando os navios do país {0}...".format(random.
 
 print("1: Brasil \n 1 cruzador \n 2 torpedeiro \n 1 destroyer \n 1 couracado \n 1 porta-avioes \n \n2: França \n 3 cruzador \n 1 porta-avioes \n 1 destroyer \n 1 couracado \n 1 submarino \n\n3: Austália \n 1 couracao \n 3 cruzador \n 1 submarino \n 1 torpedeiro \n 1 porta-avioes \n\n4: Rússia \n 1 cruzador \n 1 porta-avioes \n 2 couracado \n 1 destroyer \n 1 submarino \n\n5: Japão \n 2 torpedeiro \n 1 cruzador \n 2 destroyer \n 1 couracado \n 1 submarino")
 print('\n')
-input('Qual o número da nação da sua frota?')    
+n_nacao=0 #n_nacao= númerro da nação escolhida
+while n_nacao!=1 and n_nacao!=2 and n_nacao!=3 and n_nacao!=4 and n_nacao!=5:
+    n_nacao=int(input('Qual o número da nação da sua frota?'))
+    if n_nacao==1:
+        print("Você escolheu a nação {0}".format(lista_paises[n_nacao-1]))
+    elif n_nacao==2:
+        print("Você escolheu a nação {0}".format(lista_paises[n_nacao-1]))
+    elif n_nacao==3:
+        print("Você escolheu a nação {0}".format(lista_paises[n_nacao-1]))
+    elif n_nacao==4:
+        print("Você escolheu a nação {0}".format(lista_paises[n_nacao-1]))
+    elif n_nacao==5:
+        print("Você escolheu a nação {0}".format(lista_paises[n_nacao-1]))
+    else:
+        print('Opção inválida')
+print("Agora é vez de alocar seus navios de guerra!")
+
 
